254599a60eeb6215f54ffe4caa930939d52bcb24
Dmitry <dimaruzaev2@gmai.com>
2024-07-17 16:54:33 +0300

    ref modules

--- 2048.pas
+++ 2048.pas
@@ -1,41 +1,5 @@
 ﻿uses crt;
-
-procedure drawSquare(x: integer; y: integer);
-begin
-      var color := darkGray;
-      
-      gotoXY(x, y);
-      for var i := 0 to 5 do
-      begin
-            for var j := 0 to 5 do
-            begin
-                  textColor(color);
-                  textBackground(color);
-                  write('@');
-            end;
-            gotoXY(x, whereY + 1);
-      end;
-end;
-
-procedure drawField(initialX: integer; initialY: integer);
-begin
-      var x: integer;
-      var y: integer;
-      
-      x := initialX;
-      y := initialY;
-      
-      for var i := 0 to 3 do
-      begin
-            for var j := 0 to 3 do
-            begin
-                  drawSquare(x, y);
-                  x := x + 7;
-            end;
-            x := initialX;
-            y := y + 7;
-      end;
-end;
+uses drawFieldModule;
 
 begin
       var a: array [0..15] of integer;
